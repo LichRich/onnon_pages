94fff7fafac3c9e9141c05e80af52584a491422c
LichRich <leech135789@gmail.com>
2022-11-30 14:58:12 +0900

    separate about page
    1. create about page
     - separate the about page from index.html

---
+++ assets/scripts/about_js.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#showcase > div').fadeIn();
+    $('#showcase > div').css('display', 'flex');
+    $('#showcase-captions').css('transform', 'translateX(4%)');
+    $('#showcase-captions').css('-webkit-transform', 'translateX(4%)');
+    $('#showcase-captions').css('-ms-transform', 'translateX(4%)');
+});
--- assets/scripts/index_js.js
+++ assets/scripts/index_js.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-    $('#showcase > div').fadeIn();
-    $('#showcase > div').css('display', 'flex');
-    $('#showcase-captions').css('transform', 'translateX(4%)');
-    $('#showcase-captions').css('-webkit-transform', 'translateX(4%)');
-    $('#showcase-captions').css('-ms-transform', 'translateX(4%)');
-
-    $('#go-search').click(function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('#section-search-bar')
-                .offset()
-                .top - $('#top-navbar').height()
-        }, 0);
-    });
-
     $('#btn-search').click(function (e) {
         e.preventDefault();
         goList(searchKeyword);
